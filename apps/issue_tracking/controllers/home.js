e3fc5f8d9898b2c06b1fcd723fb0dbd3d07917d4
n0252056 <ALEXANDER.LASIUK@LIBERTYMUTUAL.COM>
2017-06-28 09:29:26 -0400

    final changes

--- apps/issue_tracking/controllers/home.js
+++ apps/issue_tracking/controllers/home.js
@@ -2,27 +2,50 @@
 	
 	angular.module('mySite').controller('issueCtrl', issueCtrl);
 
-	issueCtrl.$inject = [ "$routeParams", "mySiteData", "$window", "authentication" ]
-	function issueCtrl($routeParams, mySiteData, $window, authentication) {
+	issueCtrl.$inject = [ "$routeParams", "mySiteData", "$window", "authentication", "$http" ]
+	function issueCtrl($routeParams, mySiteData, $window, authentication, $http) {
 		var vm = this;
 		
 		vm.ticketTypes = ["JIRA", "RTC"];
 		vm.ticketType = "";
-		vm.loading = true;
+		vm.loading = false;
 		vm.loaded = false;
+		vm.ticketError = false;
+		apiStory = "";
+		vm.storyNum = "";
+		vm.ticket = {}
 		
 		vm.submit = function(){
-			if(!vm.loaded){
-				//make api call
-				vm.loaded = true;
-			}
+			vm.loading = true;
+			vm.loaded = false;
+			vm.ticketError = false;
+			apiTotalStory = apiStory+vm.storyNum;
+			console.log("API: "+apiTotalStory)
+			//make api call
+			$http.get(apiTotalStory)
+				.success(function(data){
+					if(data.ownedByName){
+						vm.ticket = data;
+						vm.ticketError = false;
+						vm.loading = false;
+						vm.loaded = true;
+					}else{
+						vm.ticketError = true;
+						vm.loading = false;
+						vm.loaded = false;
+					}
+				})
+				.error(function(data, status){
+					alert("ERROR: "+JSON.stringify(data)+ " STATUS: "+status)
+					vm.loading = false;
+				});
 		}
 		
 		vm.changeTicketType = function(){
 			if(vm.ticketType == "JIRA"){
-				alert("JIRA");
+				apiStory = "http://localhost:8080/ProjTrackingService/rest/proj/flerpaderp";
 			}else if(vm.ticketType == "RTC"){
-				alert("RTC");
+				apiStory = "http://localhost:8080/ProjTrackingService/rest/proj/story?storyNum=";
 			}else{
 				alert("Unknown Type");
 			}
